e481cbfad377a4ac7b14258ec7584fa1ee058711
Leonardo Vilani <leonardovselan@gmail.com>
2026-08-31 00:23:06 -0300

    feat: new usecase

--- cmd/main.go
+++ cmd/main.go
@@ -6,30 +6,29 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 
-	"github.com/leovilani/go-study/internal/order/entity"
 	"github.com/leovilani/go-study/internal/order/infra/database"
+	"github.com/leovilani/go-study/internal/order/usecase"
 )
 
 func main() {
-	order, err := entity.NewOrder("123", 10, 2)
-	if err != nil {
-		panic(err)
-	}
-	err = order.CalculateFinalPrice()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("The final price is: %f", order.FinalPrice)
-
 	db, err := sql.Open("mysql", "root:root@tcp(mysql:3306)/orders")
 	if err != nil {
 		panic(err)
 	}
 	repository := database.NewOrderRepository(db)
-	err = repository.Save(order)
+	uc := usecase.NewCalculateFinalPriceUsecase(repository)
+
+	defer db.Close()
+
+	input := usecase.OrderInputDTO{
+		ID:    "1234",
+		Price: 100,
+		Tax:   10,
+	}
+	output, err := uc.Execute(input)
 	if err != nil {
 		panic(err)
 	}
+	println(output.FinalPrice)
 }
---
+++ internal/order/usecase/calculate_price.go
@@ -0,0 +1,45 @@
+package usecase
+
+import "github.com/leovilani/go-study/internal/order/entity"
+
+type OrderInputDTO struct {
+	ID    string
+	Price float64
+	Tax   float64
+}
+
+type OrderOutputDTO struct {
+	ID         string
+	Price      float64
+	Tax        float64
+	FinalPrice float64
+}
+
+type CalculateFinalPriceUsecase struct {
+	OrderRepository entity.OrderRepositoryInterface
+}
+
+func NewCalculateFinalPriceUsecase(orderRepository entity.OrderRepositoryInterface) *CalculateFinalPriceUsecase {
+	return &CalculateFinalPriceUsecase{OrderRepository: orderRepository}
+}
+
+func (c *CalculateFinalPriceUsecase) Execute(input OrderInputDTO) (*OrderOutputDTO, error) {
+	order, err := entity.NewOrder(input.ID, input.Price, input.Tax)
+	if err != nil {
+		return nil, err
+	}
+	err = order.CalculateFinalPrice()
+	if err != nil {
+		return nil, err
+	}
+	err = c.OrderRepository.Save(order)
+	if err != nil {
+		return nil, err
+	}
+	return &OrderOutputDTO{
+		ID:         order.ID,
+		Price:      order.Price,
+		Tax:        order.Tax,
+		FinalPrice: order.FinalPrice,
+	}, nil
+}
